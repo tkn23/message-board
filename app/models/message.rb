3d94a085ac4e0caeb517adaf3ee427cac292da45
tkn23 <jun.tkn@gmail.com>
2016-03-02 14:42:35 +0000

    validated age

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
+  # 年齢は整数で1以上で入力受付
+  validates :age , numericality: { only_integer: true, greater_than: 0 }
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
